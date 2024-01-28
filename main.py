f47e00a09875b83364c605748e50030822a3f0f9
ow-sam <sam@overwatchdata.io>
2024-01-27 21:01:12 -0500

    Created InstaDMBot

---
+++ main.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from pages import Bot
+import os
+from absl import app
+from absl import flags
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string("username", None, "Username")
+flags.DEFINE_string("password", None, "Password")
+flags.DEFINE_string("users_file", "usernames.txt", "Users in a .txt file")
+flags.DEFINE_string("message_file", "message.txt", "Message in a .txt file")
+
+flags.mark_flag_as_required("username")
+flags.mark_flag_as_required("password")
+
+def main(argv):
+    del argv
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
+
+    usr =  FLAGS.username
+    passwd =  FLAGS.password
+
+    with open(os.path.join(FLAGS.users_file), 'r') as f:
+        users = f.read().splitlines()
+
+    with open(os.path.join(FLAGS.message_file), 'r') as f:
+        message = f.read()
+
+    bot = Bot(browser)
+    bot.login(usr, passwd)
+    bot.send_messages(users, message)
+
+if __name__ == '__main__':
+    app.run(main)
